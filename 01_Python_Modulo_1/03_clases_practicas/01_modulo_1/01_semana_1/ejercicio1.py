d3c656ba91e74acae8d1d9b6d0150d2addd15aca
luisguillermomolero <luisguillermomolero@gmail.com>
2025-10-21 18:51:10 -0500

    commit

---
+++ 01_Python_Modulo_1/03_clases_practicas/01_modulo_1/01_semana_1/ejercicio1.py
@@ -0,0 +1,33 @@
+#  Captura por teclado
+
+nombre = input("Regalame tu nombre: ")
+print("Tu nombre entonces es: " + nombre)
+
+# Variables, Tipos de datos y operadores
+
+nombre = "luis"
+telefono = "123456789"
+direccion = "Cll 3 # 45-65"
+correo = "luis@gmail.com"
+edad = 50
+
+print("Mi nombre es: " + nombre + " y mi telefono es " + telefono + ", tengo una dirección que es " + direccion)
+print(f"Mi nombre es: {nombre} y mi telefono es {telefono}, tengo una dirección que es {direccion}")
+
+# Datos enteros
+numero = 0
+numero_decimal = 5.0
+saludo = "Hola a todos"
+Datos_tipo_logico = True # ó False
+
+# Operadores aritmeticos
+
+a = 8
+b = 10
+
+c, d = 8, 10
+
+print(f"Suma = {a + b}")
+print(f"Suma = {c + d}")
+
+#Desarrollar la -, *, /, //, **
